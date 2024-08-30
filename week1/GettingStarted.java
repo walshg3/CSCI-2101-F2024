b1103b6620bbbf272d97d3729bad9c2b65455575
walshg3 <walshg3@go.stockton.edu>
2024-08-30 18:33:09 -0400

    GettingStarted

---
+++ week1/GettingStarted.java
@@ -0,0 +1,9 @@
+package week1;
+
+public class GettingStarted {
+    // Stinky Cheese
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("This is our first Java program!");
+    }
+}
