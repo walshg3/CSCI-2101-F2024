7d2f7a58348a3fe897ef2f75088ba78e72cd9fec
walshg3 <walshg3@go.stockton.edu>
2024-09-17 19:36:39 -0400

    added lecture notes

--- week1/Examples.java
+++ week1/Examples.java
@@ -1,5 +1,39 @@
-package week1;
+package week1; // This is a package declaration. It is the same name as the folder this file is in.
 
-public class Examples {
+public class Examples { // This is a class declaration. It is the same name as the file.
+    // In java '//' is used for single line comments.
+    // This is a comment
+
+    public static void main(String[] args) { // This is the main method
+
+        System.out.println("Hello World!"); // This is a print statement
+        // inside the statement is an expression (System.out.println())
+        // "Hello World!" is a string literal => Java Data Type
+        
+        System.out.println(5);
+
+        System.out.println(10 / 5);
+
+
+        // JAVA DATA TYPES
+        // int, double, boolean, char, String
+
+        // int -> integer | 1,2,3,4,5,6,7,8,9,10
+        // double -> decimal | 1.0, 2.0, 3.0, 4.0, 5.0, 5.5
+        // boolean -> true or false | yes, no | 1, 0 - "Fork in the road example" (if statements)
+        // char -> single character | 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'
+        // String -> sequence of characters | "Hello World!", "This is a string"
+        // note: char is in single quotes, String is in double quotes
+
+        // JAVA PRIMITIVE DATA TYPES
+        // PRIMITIVE DATA TYPES are the most basic data types available within the Java language.
+        // IE They are built into Java
+        // int, double, boolean, char
+
+        // Non-Primitive Data Types
+        // Non-primitive data types are called reference types because they refer to objects.
+        // String is a non-primitive data type
+    }
     
+
 }
