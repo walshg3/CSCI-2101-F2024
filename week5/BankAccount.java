f31a51692b0395d80975dbabe99889877fd10a5a
walshg3 <walshg3@go.stockton.edu>
2024-10-17 21:09:11 -0400

    more bank account

--- week5/BankAccount.java
+++ week5/BankAccount.java
@@ -1,5 +1,6 @@
 package week5;
 
+import java.util.Scanner; // Import the Scanner class from the java.util package to allow for user input
 /*
      * Stockton's Bank
      * Bank Account Parameters:
@@ -15,6 +16,7 @@ package week5;
      *  Getters and Setters
      *  Deposit
      *  Withdraw
+     *  Monthly Fee
      *  Transfer (Homework)
      *  Balance 
      *  Menu
@@ -31,21 +33,22 @@ package week5;
 public class BankAccount {
 
     // Bank Account Parameters (Also Called Arguments)
-    public String name;
-    private double balance;
-    private String dob;
-    private int ssn;
-    private int accountNumber;
+    public String name; // this.name
+    private double balance; // this.balance
+    private String dob; // this.dob
+    private int ssn; // this.ssn
+    private int accountNumber; // this.accountNumber
     // interestRate (interest)
     // period (interest)
     
     // Bank Account Constructor
-    public BankAccount(String name, int ssn, double balance, int accountNumber, String dob){
+    public BankAccount(String name, double balance, String dob, int ssn,  int accountNumber){
         this.name = name;
-        this.ssn = ssn;
         this.balance = balance;
-        this.accountNumber = accountNumber;
         this.dob = dob;
+        this.ssn = ssn;
+        this.accountNumber = accountNumber;
+        
     }
 
     public BankAccount(String name, int ssn){
@@ -53,5 +56,71 @@ public class BankAccount {
         this.ssn = ssn;
     }
 
+    // Getters and Setters
+    
+    public String getName() {
+        return this.name;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public String getDob() {
+        return this.dob;
+    }
+
+    public int getSSN() {
+        return this.ssn;
+    }
+
+    public int getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    // Deposit
+    public void deposit(double amount){
+        this.balance += amount; // this.balance = this.balance + amount;
+    }
+
+    // Withdraw
+    public void withdraw(double amount){
+        this.balance -= amount; // this.balance = this.balance - amount;
+    }
+
+    // Monthly Fee - will subtract a monthly fee from the balance
+    // take in a double as a parameter
+    // subtract the fee from the balance
+    // Netflix subscription fee - $12.99
+    public void monthlyFee(double fee){
+        withdraw(fee);
+    }
+
+    // print Balance
+    public void printBalance(){
+        System.out.println("Balance: $" + this.balance);
+    }
+
+    // Create a method that will print a menu to the user
+    // This is called a helper method / function
+    public static int menu() {
+        int choice;
+        Scanner input = new Scanner(System.in); // Create a scanner object
+        System.out.println("Welcome to the Bank of Stockton");
+        System.out.println("Please select an option:");
+        System.out.println("1. Deposit");
+        System.out.println("2. Withdraw");
+        System.out.println("3. Print Balance");
+        choice = input.nextInt(); // Collect the user's input
+        return choice; // Return the user's input
+    }
+
+    // Create a method that will interact with the user based on their 
+    // choice from the menu.
+    // This method will take a BankAccount object as a parameter
 
 }
--- week5/BankAccountTester.java
+++ week5/BankAccountTester.java
@@ -4,12 +4,34 @@ public class BankAccountTester {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        BankAccount bankTest = new BankAccount("John", 867530911, 100, 0001, "10/15/24");
+        BankAccount bankTest = new BankAccount("John", 100, "10/15/24", 867530911, 0001);
 
         BankAccount bankTest2 = new BankAccount("Joe", 123456789);
 
-        System.out.println(bankTest.name);
+        System.out.println(bankTest.getSSN());
+        System.out.println(bankTest.getDob());
+        System.out.println(bankTest.getName());
+        System.out.println(bankTest.getAccountNumber());
 
+        System.out.println("==================");
+        bankTest.deposit(100);
+        System.out.println(bankTest.getBalance());
+        bankTest.withdraw(199);
+        System.out.println(bankTest.getBalance());
+        bankTest.withdraw(5);
+        System.out.println(bankTest.getBalance());
+        bankTest.deposit(500);
+        System.out.println(bankTest.getBalance());
+
+        System.out.println("==================");
+        System.out.println("Monthly Fee: Neflix $12.99");
+        bankTest.monthlyFee(12.99);
+        bankTest.printBalance();
+        System.out.println("==================");
+        
+        int userChoice = BankAccount.menu(); // This is a static method, so we can call it without creating an object
+
+        System.out.println("User Choice: " + userChoice);
 
     }
 }
