36f8f2cc458760e95bb8f0169d8d9e1dad4e7467
walshg3 <walshg3@go.stockton.edu>
2024-11-12 18:56:54 -0500

    finish hard loops

--- week6/hardLoops.java
+++ week6/hardLoops.java
@@ -38,10 +38,87 @@ public class hardLoops {
         return result;
     }
 
+    /*
+     * Create a method that will check if a string is a palindrome
+     * A palindrome is a word that is spelled the same forwards and backwards
+     * Ex. racecar, mom, dad, radar
+     * 
+     * isPalindrome("racecar") → true
+     * isPalindrome("hello") → false
+     * isPalindrome("mom") → true
+     * isPalindrome("dad") → true
+     * isPalindrome("radar") → true
+     * 
+    */
+    public static boolean isPalindrome(String str){
+        boolean result = true;
+        for (int i = 0; i < str.length() / 2; i++) {
+            if(str.charAt(i) != str.charAt(str.length() - 1 - i)){
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    /*
+     * Create the isPalindrome method using a while loop 
+    */
+    public static boolean isPalindromeWhile(String str){
+        boolean result = true;
+        int i = 0;
+        while(i < str.length() / 2){
+            if(str.charAt(i) != str.charAt(str.length() - 1 - i)){
+                result = false;
+            }
+            i++;
+        }
+        return result;
+    }
+
+    /*
+     * Create a method that will count the number of times 'xx' appears in a string then return 
+     * the number of times it appears.
+     * EX.
+     * countXX("abcxx") → 1
+     * countXX("xxx") → 2
+     * countXX("xxxx") → 3
+     * countXX("abc") → 0
+     *  
+    */
+    public static int countXX(String str){
+        int result = 0;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if(str.charAt(i) == 'x' && str.charAt(i + 1) == 'x')
+            result ++;
+        }
+        return result;
+    }
+
+    public static int countXX2(String str){
+        int result = 0;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if(str.substring(i,i+2).equals("xx")){
+                result++;
+            }
+        }
+        return result;
+    }
+
+    
+
     public static void main(String[] args) {
         System.out.println("Start of harder Loop examples");
         System.out.println(everyNth("Miracle", 2)); // Mrce
         System.out.println(oneTwo("abc")); // bca
         System.out.println(oneTwo("tcagdo")); // catdog
+
+        System.out.println(isPalindrome("racecar")); // true
+        System.out.println(isPalindrome("hello")); // false
+        System.out.println(isPalindromeWhile("mom")); // true
+        System.out.println(isPalindromeWhile("jasdjas")); // false
+
+        System.out.println(countXX("abcxx")); // 1
+        System.out.println(countXX("xxx")); // 2
+        System.out.println(countXX("axxbxxcxx"));
     }
 }
